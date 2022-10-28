adea7662dd5fcc2432d872eea3e594d046c0e907
salih <muhammetsalihaslan@gmail.com>
2022-10-28 18:42:04 +0300

    rightbar

--- socialmediapp/src/components/rightbar/Rightbar.jsx
+++ socialmediapp/src/components/rightbar/Rightbar.jsx
@@ -9,6 +9,19 @@ const Rightbar = () => {
                     <img className="birthdayImg" src="assets/share/7.jpeg" alt="" />
                     <span> <b>Mahmut Tunç</b>  and <b>3 other friends</b>  have a birtday today</span>
                 </div>
+                <img className="rightbarAd"  src="assets/share/9.jpeg" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+
+                    <div className="rightbarProfileImgContainer">
+                        <img className="rightbarProfileImg" src="assets/person/4.jpeg" alt="" />
+                        <span className="rightbarOnline"></span>
+                    </div>
+                       <span className="rightbarUsername">Mahsun Redrose</span>
+                        
+                    </li>
+                </ul>
             </div>
             
         </div>
