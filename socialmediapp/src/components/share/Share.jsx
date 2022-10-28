0ccb174f5ec30d1df9667f2ae68e4525ac56f2f9
salih <muhammetsalihaslan@gmail.com>
2022-10-28 11:15:48 +0300

    share css jsx

--- socialmediapp/src/components/share/Share.jsx
+++ socialmediapp/src/components/share/Share.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./share.css"
 import PermMediaIcon from '@mui/icons-material/PermMedia';
-
+import LabelIcon from '@mui/icons-material/Label';
+import RoomIcon from '@mui/icons-material/Room';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 const Share = () => {
   return (
     <div className="share">
@@ -13,10 +15,34 @@ const Share = () => {
             <hr className="shareHr"/>
             <div className="shareBottom">
                 <div className="shareOptions">
+                    <div className="shareOption">
+
                     <PermMediaIcon className="shareIcon"/>
                     <span className="shareOptionText">
                         Photo or Video
                     </span>
+                    </div>
+                    <div className="shareOption">
+
+                    <LabelIcon className="shareIcon"/>
+                    <span className="shareOptionText">
+                      Tag
+                    </span>
+                    </div>
+                    <div className="shareOption">
+
+                    <RoomIcon className="shareIcon"/>
+                    <span className="shareOptionText">
+                        Locations
+                    </span>
+                    </div>
+                    <div className="shareOption">
+
+                    <EmojiEmotionsIcon className="shareIcon"/>
+                    <span className="shareOptionText">
+                        Feeling
+                    </span>
+                    </div>
                 </div>
 
             </div>
