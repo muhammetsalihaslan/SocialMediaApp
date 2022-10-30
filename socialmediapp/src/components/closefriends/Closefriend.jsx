fa20ac0b8bfab2155f5a70f627c694d9197c94db
salih <muhammetsalihaslan@gmail.com>
2022-10-30 22:27:41 +0300

    closefriend

--- socialmediapp/src/components/closefriends/Closefriend.jsx
+++ socialmediapp/src/components/closefriends/Closefriend.jsx
@@ -3,18 +3,20 @@ import "./closefriend.css";
 
 const Closefriend = ({user}) => {
   return (
-     <li className="sidebarFriends">
+     
+     <>
+     
+     <img className="closeFriendImg" src={user.profilePicture} alt="" />
+     <span className="closeFriendName">{user.username}</span>
+     
+     
+     </>
+       
 
-       <div className="sidebarFriend">
-
-        <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
-        <span className="sidebarFriendName">{user.username}</span>
-
-       </div>
-        
-
-     </li>
   )
+       
+
+     
 }
 
 export default Closefriend
