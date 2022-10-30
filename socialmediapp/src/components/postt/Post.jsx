8180192e578f12e72e3fc1254c7218ffb3c21d6b
salih <muhammetsalihaslan@gmail.com>
2022-10-30 12:13:49 +0300

    post functionalty

--- socialmediapp/src/components/postt/Post.jsx
+++ socialmediapp/src/components/postt/Post.jsx
@@ -4,8 +4,16 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import { Users } from "../../data"
+import { useState } from "react";
 
 const Post = ({post}) => {
+  const [like, setLike] = useState(post.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler =() => {
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked)
+  }
   
   
 
@@ -28,9 +36,10 @@ const Post = ({post}) => {
         </div>
         <div className="postBottom">
             <div className="postBottomLeft">
-                <FavoriteIcon htmlColor="tomato" className="likeIcon"/>
-                <ThumbUpAltIcon htmlColor="blue" className="likeIcon"/>
-                <span className="postLikeCounter">{post.like} liked it</span>
+                <FavoriteIcon htmlColor="tomato" className="likeIcon" onClick={likeHandler}/>
+                <ThumbUpAltIcon htmlColor="blue" className="likeIcon"
+                onClick={likeHandler}/>
+                <span className="postLikeCounter">{like} liked it</span>
             </div>
             <div className="postBottomRight">
                 <span className="postCommentText">{post.comment} comments</span>
