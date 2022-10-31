d569dbabc8f11282ced8a8ea4ddff6a19a7302a1
salih <muhammetsalihaslan@gmail.com>
2022-10-31 11:56:43 +0300

    logincss

--- socialmediapp/src/pages/login/Login.jsx
+++ socialmediapp/src/pages/login/Login.jsx
@@ -10,7 +10,13 @@ const Login = () => {
                 <span className="loginDesc" >Connect with your friends and other</span>
             </div>
             <div className="loginRight">
-                <div className="loginBox"></div>
+                <div className="loginBox">
+                    <input placeholder="Email" className="loginInput" />
+                    <input placeholder="password" className="loginInput" />
+                    <button className="loginButton">Log In</button>
+                    <span className="loginForgot">Forgot Password</span>
+                    <button className="loginRegisterButton">Create a New Account</button>
+                </div>
             </div>
 
 
