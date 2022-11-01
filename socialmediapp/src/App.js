ca84fa30d3cad4d1c2824f1491d1fb43927e03a7
salih <muhammetsalihaslan@gmail.com>
2022-11-01 10:26:29 +0300

    reactrouter

--- socialmediapp/src/App.js
+++ socialmediapp/src/App.js
@@ -2,13 +2,25 @@
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Home from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
 function App() {
   return (
     <div >
-      <Home/>
+      <BrowserRouter>
+      <Routes>
+
+      <Route path="/" element={<Home/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="profile" element={<Profile/>}/>
+
+
+      </Routes>
+      
+      
+      </BrowserRouter>
     </div>
   );
 }
